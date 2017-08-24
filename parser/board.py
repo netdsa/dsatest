netdsa/dsatest

commit f48086a10e611c1916d934ad0f879a168c4cd168

parser/board: Name switch more explicitely

parser/board.py
@@ -80,7 +80,9 @@ class BoardParser:
         if switch_name is None or len(interfaces) == 0:
             raise ValueError("Missing switch name or port information")
 
-        s = SwitchParser(switch_name)
+        # Make the switch name to be "model#id"
+        full_switch_name = switch_name + "#" + switch_id
+        s = SwitchParser(full_switch_name)
         for i in interfaces:
             i.switch = s
         self.interfaces.extend(interfaces)

commit c2b688e34b701c2313477e9b344b795cfa85cc29

parser/switch: use the right string to find the config file
A previous commit changed the way switches are named. If a switch
defined like this in the board was file was foud

  [switch0]
  name = "wag200g"

It would create a switch named wag200g#0. Instead of using the "name"
key directly as a key to locate the config file, it used the generated
name, which resulted in an error.

The switch parser is now updated to take both its full name and the
model as parameter, so that the right file can be found.

(Fixes: f48086a10 "parser/board: Name switch more explicitely")

parser/board.py
@@ -82,7 +82,7 @@ class BoardParser:
 
         # Make the switch name to be "model#id"
         full_switch_name = switch_name + "#" + switch_id
-        s = SwitchParser(full_switch_name)
+        s = SwitchParser(full_switch_name, switch_name)
         for i in interfaces:
             i.switch = s
         self.interfaces.extend(interfaces)

parser/switch.py
@@ -3,8 +3,8 @@ from squidsa.helper.resources import Resource
 
 class SwitchParser:
 
-    def __init__(self, switch_name):
+    def __init__(self, switch_name, model):
         self.name = switch_name
 
-        switch_cfg = Resource(Resource.SWITCH, switch_name).get_path()
+        switch_cfg = Resource(Resource.SWITCH, model).get_path()
         self.config = switch_cfg