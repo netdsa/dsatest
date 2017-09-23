baefa9f8b31af8835da7352bda642b381f0b093d
Damien Riegel <damien.riegel@savoirfairelinux.com>
2017-09-23 12:31:29 -0400

    bench: rename env.py to bench.py
    As for previous commit, get rid of old semantic.

--- bench/__init__.py
+++ bench/__init__.py
@@ -2,7 +2,7 @@
 __all__ = [ 'Bench', 'Bridge', 'Interface', 'Link', 'Machine' ]
 
 from .bridge import Bridge
-from .env import Bench
+from .bench import Bench
 from .interface import Interface
 from .link import Link
 from .machine import Machine
