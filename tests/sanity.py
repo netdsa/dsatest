7e282afbf28d0a234c97b4d65d358fd055be380a
Damien Riegel <damien.riegel@savoirfairelinux.com>
2017-11-10 11:05:59 -0500

    tests/sanity: skip tests on target if not connected
    The point of sanity tests is to be able to verify that `dsatest` hasn't
    broken in an easy and obvious way. `--dry-run` always to verify that
    without being connected to an actual target, but echo tests on the
    target will fail if the bench is not connected.
    
    Fixes: e333c79fb "tests/sanity: add echo tests"

--- tests/sanity.py
+++ tests/sanity.py
@@ -68,11 +68,13 @@ class TestSanity(unittest.TestCase):
         self.assertTrue(hasattr(b, "addAddress"))
         self.assertTrue(hasattr(b, "delAddress"))
 
+    @unittest.skipIf(not bench.target.control.isConnected(), "Not available in dry-run mode")
     def test_sanity_echo_target(self):
         ret, stdout, stderr = bench.target.exec("echo -n 'Hello World'")
         self.assertEqual(stdout.strip(), "Hello World")
         self.assertEqual(stderr.strip(), "")
 
+    @unittest.skipIf(not bench.target.control.isConnected(), "Not available in dry-run mode")
     def test_sanity_echo_target_stderr(self):
         ret, stdout, stderr = bench.target.exec("echo -n 'Hello World' >&2")
         self.assertEqual(stdout.strip(), "")
