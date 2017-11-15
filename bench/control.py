netdsa/dsatest

commit 8f6d730b5fffa8a417bb58b4b43e76a3a0b55066

bench/control: factorize `execute` function
Each subclass had the logging code duplicated. This can be factorized
in the base class, and this had the advantage that logging is
guaranteed to be consistent across subclasses.

bench/control.py
@@ -12,11 +12,8 @@ logger = logging.getLogger('dsatest')
 
 class Control(object):
 
-    def execute(self):
-        raise NotImplementedError()
-
     def getLastExitCode(self):
-        raise NotImplementedError()
+        return self.getLastExitCode
 
     def isConnected(self):
         raise NotImplementedError()
@@ -26,6 +23,29 @@ class Control(object):
         if actual_exit_code != expected_exit_code:
             raise ValueError("Exit code mismatch. Got {}, expected {}".format(
                 actual_exit_code, expected_exit_code))
+
+    def _execute(self, command):
+        raise NotImplementedError()
+
+    def execute(self, command):
+        """
+        Wrap subclasses' execute functions with a logging facility. That way,
+        logging is factorized and consistent for all of them.
+        """
+        class_name = self.__class__.__name__
+        logger.debug("%s: Executing: %s", class_name, command)
+
+        self.exit_code, stdout, stderr = self._execute(command)
+
+        logger.debug("%s: Command returned %d", class_name, self.exit_code)
+        if stdout != '':
+            for line in stdout.split('\n'):
+                logger.debug("%s: stdout: %s", class_name, line)
+        if stderr != '':
+            for line in stderr.split('\n'):
+                logger.debug("%s: stderr: %s", class_name, line)
+
+        return self.exit_code, stdout, stderr
 
     def execAndCheck(self, command, expected_exit_code=0):
         self.execute(command)
@@ -40,34 +60,22 @@ class LocalControl(Control):
     def isConnected(self):
         return True
 
-    def execute(self, command):
-        logger.debug("LocalControl: Executing: {}".format(command))
+    def _execute(self, command):
         if sys.version_info[0] >= 3:
             ret = subprocess.run(command, shell=True,
                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            self.exit_code = ret.returncode
+            exit_code = ret.returncode
             stdout = ret.stdout.decode(sys.stdout.encoding).strip()
             stderr = ret.stderr.decode(sys.stderr.encoding).strip()
         else:
             ret = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
             ret.wait()
-            self.exit_code = ret.returncode
+            exit_code = ret.returncode
             stdout = ret.stdout.read().strip()
             stderr = ret.stderr.read().strip()
 
-        logger.debug("LocalControl: Command returned {}".format(self.exit_code))
-        if stdout != '':
-            for l in stdout.split('\n'):
-                logger.debug("LocalControl: stdout: {}".format(l))
-        if stderr != '':
-            for l in stderr.split('\n'):
-                logger.debug("LocalControl: stderr: {}".format(l))
-
-        return self.exit_code, stdout, stderr
-
-    def getLastExitCode(self):
-        return self.exit_code
+        return exit_code, stdout, stderr
 
 
 class SSHControl(Control):
@@ -149,23 +157,14 @@ class SSHControl(Control):
         return ret
 
 
-    def execute(self, command):
+    def _execute(self, command):
         """Execute a command on a machine, using SSH"""
-        self.exit_code = None
-        logger.debug("SSHControl: Executing: {}".format(command))
         _, stdout, stderr = self.ssh_client.exec_command(command)
-        self.exit_code = stdout.channel.recv_exit_status()
-        logger.debug("SSHControl: Command returned {}".format(self.exit_code))
+        exit_code = stdout.channel.recv_exit_status()
         stdout = stdout.read().decode().strip()
         stderr = stderr.read().decode().strip()
-        if stdout != '':
-            for l in stdout.split('\n'):
-                logger.debug("SSHControl: stdout: {}".format(l.strip()))
-        if stderr != '':
-            for l in stderr.split('\n'):
-                logger.debug("SSHControl: stderr: {}".format(l.strip()))
 
-        return self.exit_code, stdout, stderr
+        return exit_code, stdout, stderr
 
     def getLastExitCode(self):
         return self.exit_code
@@ -233,14 +232,12 @@ class TelnetControl(Control):
             ret.append(arg)
         return ret
 
-    def execute(self, command):
+    def _execute(self, command):
         """Execute a command on a machine, using Telnet"""
-        self.exit_code = None
         """
         Confirm the command was correctly echoed back and then ask for
         its return code
         """
-        logger.debug("TelnetControl: Executing: {}".format(command))
         self.telnet_client.write((command + "\r\n").encode())
         resp = self.telnet_client.read_until((command + "\r\n").encode())
         while True:
@@ -253,17 +250,8 @@ class TelnetControl(Control):
         self.telnet_client.write("echo $?\r\n".encode())
         _, match, _ = self.telnet_client.expect([re.compile(br'(\d+)')],
                                                 TelnetControl.TELNET_TIMEOUT)
-        self.exit_code = int(match.group(1).decode())
-        logger.debug("TelnetControl: Command returned {}".format(self.exit_code))
+        exit_code = int(match.group(1).decode())
 
-        if self.exit_code != 0:
+        if exit_code != 0:
             stderr = resp.decode()
-        for l in stdout.split('\n'):
-            logger.debug("TelnetControl: stdout: {}".format(l.strip()))
-        for l in stderr.split('\n'):
-            logger.debug("TelnetControl: stderr: {}".format(l.strip()))
-
-        return self.exit_code, stdout, stderr
-
-    def getLastExitCode(self):
-        return self.exit_code
+        return exit_code, stdout, stderr

commit b0c48ab8377ce4e473ccaacff5701c785b7d6b4f

bench/control: make checkExitCode take last exit code as argument
Instead of storing the last exit code as class attribute, just pass it
to the function in charge of checking if the command succeeded or not.
As it doesn't use "self" anymore, it can be made static.

getLastExitCode is dropped as it's not used anymore. This function was
broken during the refactoring of the "execute" function.

Fixes: 8f6d730b5f "bench/control: factorize `execute` function"

bench/control.py
@@ -12,17 +12,8 @@ logger = logging.getLogger('dsatest')
 
 class Control(object):
 
-    def getLastExitCode(self):
-        return self.getLastExitCode
-
     def isConnected(self):
         raise NotImplementedError()
-
-    def checkExitCode(self, expected_exit_code):
-        actual_exit_code = self.getLastExitCode()
-        if actual_exit_code != expected_exit_code:
-            raise ValueError("Exit code mismatch. Got {}, expected {}".format(
-                actual_exit_code, expected_exit_code))
 
     def _execute(self, command):
         raise NotImplementedError()
@@ -35,9 +26,9 @@ class Control(object):
         class_name = self.__class__.__name__
         logger.debug("%s: Executing: %s", class_name, command)
 
-        self.exit_code, stdout, stderr = self._execute(command)
+        exit_code, stdout, stderr = self._execute(command)
 
-        logger.debug("%s: Command returned %d", class_name, self.exit_code)
+        logger.debug("%s: Command returned %d", class_name, exit_code)
         if stdout != '':
             for line in stdout.split('\n'):
                 logger.debug("%s: stdout: %s", class_name, line)
@@ -45,11 +36,17 @@ class Control(object):
             for line in stderr.split('\n'):
                 logger.debug("%s: stderr: %s", class_name, line)
 
-        return self.exit_code, stdout, stderr
+        return exit_code, stdout, stderr
+
+    @staticmethod
+    def _checkExitCode(exit_code, expected_exit_code):
+        if exit_code != expected_exit_code:
+            raise ValueError("Exit code mismatch. Got {}, expected {}".format(
+                exit_code, expected_exit_code))
 
     def execAndCheck(self, command, expected_exit_code=0):
-        self.execute(command)
-        self.checkExitCode(expected_exit_code)
+        exit_code, _, _ = self.execute(command)
+        self._checkExitCode(exit_code, expected_exit_code)
 
     def search_for_config(self, cfg_section, options):
         """
@@ -174,9 +171,6 @@ class SSHControl(Control):
         stderr = stderr.read().decode().strip()
 
         return exit_code, stdout, stderr
-
-    def getLastExitCode(self):
-        return self.exit_code
 
 
 class TelnetControl(Control):