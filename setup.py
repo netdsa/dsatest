2613e064e7e7f583a855a73ac69bf20febe67fc7
Damien Riegel <damien.riegel@savoirfairelinux.com>
2017-12-20 13:58:23 -0500

    add installer script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dsatest",
+    version="0.0.1-alpha",
+    author="Damien Riegel",
+    author_email="damien.riegel@savoirfairelinux.com",
+    description="A Linux network testing framework for hardware switching devices",
+    packages=find_packages(),
+    package_data={
+        # Include config files:
+        '': ['*.cfg',],
+    },
+    entry_points={
+        'console_scripts': ['dsatest = dsatest.bench.runner:main',]
+    },
+)
