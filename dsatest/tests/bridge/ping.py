e1a91c3c28f25bccc2609d999e6c8661668c9d9f
Damien Riegel <damien.riegel@savoirfairelinux.com>
2018-01-05 12:37:18 -0500

    tests/bridge: ping merge setUp/tearDown in test function
    In order to be able to add other test methods to TestBridge, specific
    setUp and tearDown must be moved within `test_bridge_ping_one`. Use
    this small refactoring to rename it to `test_bridge_ping_all_interfaces_bridged`.

--- dsatest/tests/bridge/ping.py
+++ dsatest/tests/bridge/ping.py
@@ -7,27 +7,18 @@ from dsatest.tests.helpers import up_and_wait
 @unittest.skipIf(not bench.links, "test requires at least 1 link")
 class TestBridge(unittest.TestCase):
 
-    def setUp(self):
-        self.bridge = bench.target.add_bridge("br0")
-        self.bridge.up()
+    def test_bridge_ping_all_interfaces_bridged(self):
+        bridge = bench.target.add_bridge("br0")
+        bridge.up()
 
-        self.bridge.add_address("192.168.10.1/24")
+        # setup: add all target interfaces within the Bridge
+        bridge.add_address("192.168.10.1/24")
         for link in bench.links:
             link.host_if.flush_addresses()
-            self.bridge.add_interface(link.target_if)
+            bridge.add_interface(link.target_if)
             up_and_wait(link)
 
-
-    def tearDown(self):
-        for link in bench.links:
-            self.bridge.del_interface(link.target_if)
-            link.host_if.down()
-            link.target_if.down()
-        self.bridge.down()
-        bench.target.del_bridge(self.bridge)
-
-
-    def test_bridge_ping_one(self):
+        # test: make all host interfaces ping the bridge IP
         for i, link in enumerate(bench.links, start=2):
             host_addr = "192.168.10.{}/24".format(str(i))
             link.host_if.add_address(host_addr)
@@ -37,3 +28,11 @@ class TestBridge(unittest.TestCase):
             """
             link.host_if.ping("192.168.10.1", count=1)
             link.host_if.flush_addresses()
+
+        # tearDown: remove interfaces from the bridge, then delete it
+        for link in bench.links:
+            bridge.del_interface(link.target_if)
+            link.host_if.down()
+            link.target_if.down()
+        bridge.down()
+        bench.target.del_bridge(bridge)
